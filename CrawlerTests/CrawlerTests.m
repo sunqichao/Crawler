92e5a8384290b30a51c64013c280ec6b33726c60
sunqichao <sunqichaojob@gmail.com>
2014-06-13 16:26:28 +0800

    Initial Commit

---
+++ CrawlerTests/CrawlerTests.m
@@ -0,0 +1,34 @@
+//
+//  CrawlerTests.m
+//  CrawlerTests
+//
+//  Created by MacBook on 14-6-13.
+//  Copyright (c) 2014年 sqc. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface CrawlerTests : XCTestCase
+
+@end
+
+@implementation CrawlerTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
